96864552a646ca13c979aa466416feb0eb713b2f
dilbarhussain <82407752+dilbarhussainmalik12345@users.noreply.github.com>
2023-03-09 11:35:22 -0800

    conversion of jpg file to PNG
    python code to convert jpg file to png

---
+++ LabTask_jpg_to_PNG.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from PIL import Image
+im1 = Image.open('E:\\Memories\\HEC visit with Sir Muzamil\\131ND750\\sir.JPG')
+im1.save('E:\\Memories\\HEC visit with Sir Muzamil\\131ND750\\new_sir.PNG')
+
+
+# In[ ]:
+
+
+
+
